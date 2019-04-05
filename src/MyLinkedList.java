e1cc48c894d677c7d0a353c39f20448e49321c21
Ravie <ravijkee@yandex.ru>
2019-04-05 06:58:14 +0300

    testing code

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -275,4 +275,20 @@ public class MyLinkedList<E> implements ILinkedList<E> {
         }
 
     }
+
+    public String toString() {
+        Iterator<E> it = iterator();
+        if (! it.hasNext())
+            return "[]";
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (;;) {
+            E e = it.next();
+            sb.append(e == this ? "(this Collection)" : e);
+            if (! it.hasNext())
+                return sb.append(']').toString();
+            sb.append(',').append(' ');
+        }
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,40 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        LinkedList<Integer> linkedList2 = new LinkedList<>();
+        Long startTime, endTime;
 
-        linkedList.add(1);
-        linkedList.add(2);
-        linkedList.add(3);
-        linkedList.add(4);
-        linkedList2.add(0);
-        linkedList2.add(0);
-        linkedList.addAll(0, linkedList2);
+        List<Integer> collection = new ArrayList<>();
+        for(int i = 1; i < 10; i++) {
+            collection.add(-i);
+        }
+
+
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for(int i = 0; i < 10; i++) {
+            linkedList.add(i);
+        }
+        startTime = System.nanoTime();
+        linkedList.addAll(0, collection);
+        endTime = System.nanoTime();
+        System.out.println("LinkedList: " + (endTime - startTime) + " ns");
+        System.out.println(linkedList.toString());
+
+        startTime = System.nanoTime();
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+        for(int i = 0; i < 10; i++) {
+            myLinkedList.add(i);
+        }
+        startTime = System.nanoTime();
+        myLinkedList.addAll(0, collection);
+        endTime = System.nanoTime();
+        System.out.println("MyLinkedList: " + (endTime - startTime) + " ns");
+        System.out.println(myLinkedList.toString());
+
+
+
     }
 }
